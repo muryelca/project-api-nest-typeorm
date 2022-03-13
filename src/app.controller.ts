8e69d9e5d63513fc5254a8f405b117785b30674e
muryelca <muryel_msn@hotmail.com>
2022-03-12 21:51:28 -0300

    add: services to user, login, product and sell

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Controller, Get, Post, Delete, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('teste')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('usuario')
+  getUser(): string {
+    return this.appService.getUser();
+  }
+
+  @Get('login')
+  getLogin(): string {
+    return this.appService.getLogin();
+  }
+
+  @Get('produto')
+  getProduct(): string {
+    return this.appService.getProduct();
+  }
+
+  @Get('compra')
+  getSell(): string {
+    return this.appService.getSell();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,18 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'BeSafe Api';
+    return 'Teste';
+  }
+  getLogin(): string {
+    return 'Api Login';
+  }
+  getUser(): string {
+    return 'Api Usuario';
+  }
+  getProduct(): string {
+    return 'Api de Produto';
+  }
+  getSell(): string {
+    return 'Api para Vender Produto';
   }
 }
