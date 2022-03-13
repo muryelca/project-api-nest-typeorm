4f1fa14c7c285ec176be8620c5d9d8d31c547aca
muryelca <muryel_msn@hotmail.com>
2022-03-13 01:00:18 -0300

    add: database connection

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,14 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Teste';
+  getInit(): string {
+    return 'Tela Inicial';
   }
   getLogin(): string {
     return 'Api Login';
   }
   getUser(): string {
     return 'Api Usuario';
+  }
+  getUsers(): string {
+    return 'Api de todos os Usuarios';
   }
   getProduct(): string {
     return 'Api de Produto';
