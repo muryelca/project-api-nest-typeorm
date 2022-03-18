a17a1af7de76089723b34e397668dc2e82d88d5c
muryelca <muryel_msn@hotmail.com>
2022-03-17 22:45:25 -0300

    add: user service

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserPostEntity } from '../models/post.entity';
+import { UserPost } from '../models/post.interface';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserPostEntity)
+    private readonly userPostRepository: Repository<UserPostEntity>,
+  ) {}
+
+  createPost(userPost: UserPost) {
+    return this.userPostRepository.save(userPost);
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
 import { UserController } from './controllers/user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserPostEntity } from './models/post.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserPostEntity])],
   providers: [UserService],
   controllers: [UserController],
 })
