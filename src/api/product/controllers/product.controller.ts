dabe585770f481197d48be444ae4e1505d8bf521
muryelca <muryel_msn@hotmail.com>
2022-03-19 03:23:14 -0300

    add: pagination, delete, post, put in product

--- src/api/product/controllers/product.controller.ts
+++ src/api/product/controllers/product.controller.ts
@@ -1,6 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { DeleteResult, UpdateResult } from 'typeorm';
 import { ProductPost } from '../models/post.interface';
 import { ProductService } from '../services/product.service';
 
@@ -16,5 +26,26 @@ export class ProductController {
   @Get()
   findAll(): Observable<ProductPost[]> {
     return this.productService.findAllPosts();
+  }
+  @Get()
+  findSelected(
+    @Query('take') take = 1,
+    @Query('skip') skip = 1,
+  ): Observable<ProductPost[]> {
+    take = take > 15 ? 15 : take;
+    return this.productService.findPosts(take, skip);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: number,
+    @Body() productPost: ProductPost,
+  ): Observable<UpdateResult> {
+    return this.productService.updatePost(id, productPost);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number): Observable<DeleteResult> {
+    return this.productService.deletePost(id);
   }
 }
--- src/api/product/services/product.service.ts
+++ src/api/product/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { from, Observable } from 'rxjs';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { ProductPostEntity } from '../models/post.entity';
 import { ProductPost } from '../models/post.interface';
 
@@ -18,5 +18,22 @@ export class ProductService {
 
   findAllPosts(): Observable<ProductPost[]> {
     return from(this.productPostRepository.find());
+  }
+  findPosts(take = 10, skip = 0): Observable<ProductPost[]> {
+    return from(
+      this.productPostRepository
+        .findAndCount({ take, skip })
+        .then(([posts]) => {
+          return <ProductPost[]>posts;
+        }),
+    );
+  }
+
+  updatePost(id: number, productPost: ProductPost): Observable<UpdateResult> {
+    return from(this.productPostRepository.update(id, productPost));
+  }
+
+  deletePost(id: number): Observable<DeleteResult> {
+    return from(this.productPostRepository.delete(id));
   }
 }
