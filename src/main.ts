81d4be34b5bc174ab102d53098c457a5f5cc35c4
Muryel Cabral <engenharia07@siam.ind.br>
2022-03-24 11:45:47 -0300

    add: process.env

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT || 3000, function () {
-    console.log(
-      'express server listening on port %d in %s mode',
-      this.address().port,
-      app.settings.env,
-    );
-  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
