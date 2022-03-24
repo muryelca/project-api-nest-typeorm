9d24c58d55d96967bfb1dd81f59feac212f5e69f
Muryel Cabral <engenharia07@siam.ind.br>
2022-03-24 11:32:56 -0300

    add: process.env

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-//import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  //app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, function () {
+    console.log(
+      'express server listening on port %d in %s mode',
+      this.address().port,
+      app.settings.env,
+    );
+  });
 }
 bootstrap();
