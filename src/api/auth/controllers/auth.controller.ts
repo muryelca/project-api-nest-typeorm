68402d12da38e474fb4c59c9d5ec11ca5ffdcedd
muryelca <muryel_msn@hotmail.com>
2022-03-23 21:33:16 -0300

    add: JWT, AuthBearerToken, Register with 8 digits crypt.

--- src/api/auth/controllers/auth.controller.ts
+++ src/api/auth/controllers/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { UserEntity } from '../models/user.entity';
 import { User } from '../models/user.interface';
 import { AuthService } from '../services/auth.service';
 
@@ -9,7 +8,7 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  register(@Body() user: user): Observable<User> {
+  register(@Body() user: User): Observable<User> {
     return this.authService.registerAccount(user);
   }
 }
