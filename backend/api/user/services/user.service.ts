99906f06386b49991c6e94b83829f0ebf33bc30a
Muryel Cabral <muryel_msn@hotmail.com>
2023-03-13 03:29:10 -0300

    fix: git ignore

--- backend/api/user/services/user.service.ts
+++ backend/api/user/services/user.service.ts
@@ -1,42 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { from, Observable } from 'rxjs';
-import { DeleteResult, Repository, UpdateResult } from 'typeorm';
-import { UserPostEntity } from '../models/post.entity';
-import { UserPost } from '../models/post.interface';
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { from, Observable } from 'rxjs'
+import { DeleteResult, Repository, UpdateResult } from 'typeorm'
+import { UserPostEntity } from '../models/post.entity'
+import { UserPost } from '../models/post.interface'
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserPostEntity)
-    private readonly userPostRepository: Repository<UserPostEntity>,
+    private readonly userPostRepository: Repository<UserPostEntity>
   ) {}
 
   createPost(userPost: UserPost): Observable<UserPost> {
-    return from(this.userPostRepository.save(userPost));
+    return from(this.userPostRepository.save(userPost))
   }
 
   findAllPosts(): Observable<UserPost[]> {
-    return from(this.userPostRepository.find());
+    return from(this.userPostRepository.find())
   }
 
   findPosts(take = 10, skip = 0): Observable<UserPost[]> {
     return from(
       this.userPostRepository.findAndCount({ take, skip }).then(([posts]) => {
-        return <UserPost[]>posts;
-      }),
-    );
+        return <UserPost[]>posts
+      })
+    )
   }
 
   updatePost(id: number, userPost: UserPost): Observable<UpdateResult> {
-    return from(this.userPostRepository.update(id, userPost));
+    return from(this.userPostRepository.update(id, userPost))
   }
 
   deletePost(id: number): Observable<DeleteResult> {
-    return from(this.userPostRepository.delete(id));
+    return from(this.userPostRepository.delete(id))
   }
 
   findUserById(id: number): Observable<UserPost> {
-    return from(this.userPostRepository.findOne({ id }));
+    return from(this.userPostRepository.findOne({ id }))
   }
 }
--- backend/api/user/user.module.ts
+++ backend/api/user/user.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './services/user.service';
-import { UserController } from './controllers/user.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserPostEntity } from './models/post.entity';
+import { Module } from "@nestjs/common";
+import { UserService } from "./services/user.service";
+import { UserController } from "./controllers/user.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserPostEntity } from "./models/post.entity";
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserPostEntity])],
